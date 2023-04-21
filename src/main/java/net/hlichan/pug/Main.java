570340449abf8a13cf044597af49f1aa2c536cba
Hlichan <hiiragi.zzz.od@gmail.com>
2023-04-22 00:31:07 +0900

    いつものやつ作成

--- src/main/java/net/hlichan/pug/Main.java
+++ src/main/java/net/hlichan/pug/Main.java
@@ -3,15 +3,27 @@ package net.hlichan.pug;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+    private static JavaPlugin plugin;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // プラグイン起動時の処理
+        super.onEnable();
 
+        plugin = this;
+
+        getLogger().info("プラグインは正常に起動しました。");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // プラグイン停止時の処理
+        super.onDisable();
+
+        getLogger().info("プラグインは正常に停止しました。");
+    }
+
+    public static JavaPlugin getPlugin() {
+        return plugin;
     }
 }
